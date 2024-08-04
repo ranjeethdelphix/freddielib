052b67c93c9a06345b1911d1734ca09be4fa0513
ranjeethdelphix <88906215+ranjeethdelphix@users.noreply.github.com>
2024-08-03 23:22:19 -0400

    Add files via upload

---
+++ Step1.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#Script v1.0
+#############################################################
+#This script creates an inventory for Aurora Postgre Refresh
+#############################################################
+
+##argument_script.sh
+vars=$(getopt -o i:o: --long input:,output: -- "$@")
+eval set -- "$vars"
+
+# extract options and their arguments into variables.
+for opt; do
+    case "$opt" in
+      --sourcedb)
+        source_db = $2
+        shift 2
+        ;;
+      --targetdb)
+        target_db=$2
+        shift 2
+        ;;
+      --refreshtype)
+        refresh_type=$2
+        shift 2
+        ;;
+    esac
+done
+
+./pg_refresh -sdb $source_db -tdb $target_db -rt $refresh_type
+
+
+############## E O F ####################################
